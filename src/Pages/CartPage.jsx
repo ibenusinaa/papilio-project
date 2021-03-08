6cf76e96df40ff3c4dbb94e58a5a24376e6d85f8
ibenusinaa <ibenu.sina@gmail.com>
2021-03-08 09:19:54 +0700

    8maret

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -2,65 +2,13 @@ import React from 'react'
 import axios from 'axios'
 
 export default class CartPage extends React.Component{
-
-    state = {
-        productCart: null
-
-    }
-
-    componentDidMount(){
-        // this.getCart()
-    }
-
-    // getCart = () => {
-    //     let id = localStorage.getItem('id')
-    //     if(id){
-    //         axios.get(`http://localhost:2000/carts?idUser=${id}`)
-    //         .then((res) => {
-    //             console.log(res.data)
-    //             this.setState({productCart: res.data})
-    //             this.state.productCart.forEach((value, index) =>{
-    //                 let idProduct = 
-    //                 axios.get('')
-                    
-    //             })
-    //         })
-    //         .catch((err) => {
-    //             console.log(err)
-    //         })
-    //     }else{
-
-    //     }
-    // }
-
-    showCart = () => {
-        if(this.state.productCart){
-            return(
-                this.state.productCart.map((value, index) => {
-                    <div key={index} className='row my-2'>
-                        <div className ='col-4 '>
-                            <img src='https://nodaheights.com/wp-content/uploads/2017/08/Furniture-Background-Image.jpg' className='ml-3' style={{height:'100%', width:'100%'}} />
-                        </div>
-                        <div className ='col-8'>
-                            <div className='ml-3'>
-                                <h4>Nama Brand</h4>
-                                <h6 className='mt-n2'>Lampu lantai/ baca, aluminium</h6>
-                                <p>Jumlah item: 1</p>
-                                <h5>Rp800.000</h5>
-                            </div>
-                        </div>
-                    </div>
-                })
-            )
-        }
-    }
-
     render(){
         return(
             <>
                 <div className = 'bg-light'>
                     <div className ='container'>
                         <div className = 'row d-flex'>
+
                             {/* row kiri */}
                             <div className='col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8'>
                                 <div className='p-2 bg-white my-5'>
@@ -98,6 +46,7 @@ export default class CartPage extends React.Component{
                                     </div>
                                 </div>
                             </div>
+
                             {/* row kanan */}
                             <div className='col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 ' style={{height: '400px'}}>
                                 <div className='p-2 bg-white mt-5'>
